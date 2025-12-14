f9b13579c8c018d6d1412da87fbf080647dfd2b6
gith856 <deepak732196@gmail.com>
2025-12-14 06:57:56 +0530

    change image url

--- config/cloudinary.js
+++
@@ -1,9 +0,0 @@
-import { v2 as cloudinary } from "cloudinary";
-
-cloudinary.config({
-  cloud_name: dghfyvrd0,
-  api_key:358259956227189,
-  api_secret: NLQTW6tkw6c3IFaXeYZTVzShBFg,
-});
-
-export default cloudinary;
